86c9817681e0fe56382ee26ea3bd895d3ce28559
kanchan45kb <kanchan.bhosale85@gmail.com>
2025-03-06 22:48:53 +0530

    6th March Helloworld in Intelji

--- src/ex_01_Java_Basic/Helloworld.java
+++ src/ex_01_Java_Basic/Helloworld.java
@@ -1,4 +1,11 @@
 package ex_01_Java_Basic;
 
 public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("i= " +i);
+        }
+    }
 }
