fb20ffee84d5a34f5c0f3d793144cd55af654890
kanchan45kb <kanchan.bhosale85@gmail.com>
2025-03-12 08:39:40 +0530

    Math class on 12th March 2025

---
+++ src/Task1203/ex_12_arthimatic.java
@@ -0,0 +1,10 @@
+package Task1203;
+
+public class ex_12_arthimatic {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+        int z = 10;
+        System.out.println(Math.cbrt((Math.pow(x,2))+Math.pow(y,2)-Math.abs(z)));
+    }
+}
