14077a98b4ae6afabf821b3b6bd7f6262d649fa6
kanchan45kb <kanchan.bhosale85@gmail.com>
2025-03-15 02:07:09 +0530

    Lecture on 13th March increment and decrement

---
+++ src/operator/increDecre.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class increDecre {
+    public static void main(String[] args) {
+        int a = 20;
+      //  System.out.println(a++);
+        int z = a++;
+        System.out.println(z);
+        System.out.println(a);
+        int m = 15;
+        System.out.println(++m);
+        int b =++m;
+        System.out.println(b);
+        int x = 50;
+        int l = x--;
+        System.out.println(l);
+        //System.out.println(x--);
+        int n = 70;
+        System.out.println(--n);
+        int d = --n;
+        System.out.println(d);
+    }
+}
